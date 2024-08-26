f948c4333cb47a1637096013d58c8853e2cb2348
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2024-08-26 18:23:32 +0000

    added React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import styles from './App.module.css';
-import NavBar from './components/NavBar'
+import { Container } from "react-bootstrap";
+import NavBar from './components/NavBar';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
        <NavBar/>
+       <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home Page</h1>}></Route>
+          <Route exact path="/signin" render={()=> <h1>Singn in</h1>}></Route>
+          <Route exact path="/signup" render={()=> <h1>Singn up</h1>}></Route>
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+       </Container>
     </div>
   );
 }
