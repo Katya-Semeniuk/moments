ad9cdc71e29ecac49ab9992b3c0ed4b893b373ed
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2024-09-13 17:52:29 +0000

    added refresh token

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
-import React, { useContext } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo-moments.png";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
-import { CurrentUserContext } from "../App";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
-  const currentUser = useContext(CurrentUserContext);
+  const currentUser = useCurrentUser();
+
   const loggedInIcons = <>{currentUser?.username}</>
   const loggedOutIcons = (
     <>
