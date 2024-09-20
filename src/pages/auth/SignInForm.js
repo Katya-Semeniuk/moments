13a0bc16bdec355abb5f8be01ac4b7edf9817c98
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2024-09-20 14:39:44 +0000

    user redirects

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-import Form from "react-bootstrap/Form";
-import Alert from "react-bootstrap/Alert";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Image from "react-bootstrap/Image";
-import Container from "react-bootstrap/Container";
+import {Container, Form, Alert, Button, Col, Row, Image } from "react-bootstrap";
 
 import { Link, useHistory } from "react-router-dom";
 
@@ -16,10 +10,13 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { useRedirect } from "../../hooks/useRedirect";
+
 
 function SignInForm() {
 
   const setCurrentUser = useSetCurrentUser();
+  useRedirect("loggedIn");
 
   const [signInData, setSignInData] = useState({
     username: "",
@@ -35,7 +32,7 @@ function SignInForm() {
     try {
       const { data } =  await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      history.push("/");
+      history.goBack();
     } catch (err) {
       setErrors(err.response?.data);
     }
